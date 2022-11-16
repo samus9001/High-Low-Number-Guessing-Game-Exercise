1a5f7419371ad439e05273c456533072862ee9f7
Samus9001 <sameer99@outlook.com>
2022-11-16 20:49:43 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HighLowNumberGuessing
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int rndValue = rnd.Next(100);
+            int Guess = 0;
+
+            Console.WriteLine("I'm thinking of a random number between 0 and 100, can you guess it?");
+
+            while (Guess != rndValue)
+            {
+                Guess = Convert.ToInt32(Console.ReadLine());
+                if (Guess == rndValue)
+                {
+                    Console.WriteLine("You guessed the correct number!");
+                }
+
+                if (Guess > rndValue)
+                {
+                    Console.WriteLine("That number is too high, try again");
+
+                }
+
+                if (Guess < rndValue)
+                {
+                    Console.WriteLine("That number is too low, try again");
+                }
+            }
+        }
+
+    }
+}
