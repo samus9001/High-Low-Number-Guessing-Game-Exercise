398c87f1769ff0e27f41b711c8157a96342b1984
Samus9001 <sameer99@outlook.com>
2022-12-02 20:05:33 +0000

    Updated code to output if the guess is 5 off the number

--- Program.cs
+++ Program.cs
@@ -11,15 +11,29 @@ namespace HighLowNumberGuessing
             Random rnd = new Random();
             int rndValue = rnd.Next(100);
             int Guess = 0;
+            int highGuess = rndValue + 5;
+            int lowGuess = rndValue - 5;
 
             Console.WriteLine("I'm thinking of a random number between 0 and 100, can you guess it?");
+            Console.WriteLine(rndValue);
 
             while (Guess != rndValue)
             {
                 Guess = Convert.ToInt32(Console.ReadLine());
+
                 if (Guess == rndValue)
                 {
                     Console.WriteLine("You guessed the correct number!");
+                }
+
+                if (Guess == highGuess)
+                {
+                    Console.WriteLine("You're close!");
+                }
+
+                if (Guess == lowGuess)
+                {
+                    Console.WriteLine("You're close!");
                 }
 
                 if (Guess > rndValue)
