cee69be26d52a0e88be602ad735d22b6e61a2133
Samus9001 <sameer99@outlook.com>
2022-12-17 20:41:38 +0000

    Refactored and removed unnecesary code

--- Program.cs
+++ Program.cs
@@ -7,25 +7,22 @@ namespace HighLowNumberGuessing
     {
         static void Main(string[] args)
         {
-            //create a random value between 1 and 100
             Random rnd = new Random();
 
+            //create a random value between 0 and 100
             int rndValue = rnd.Next(0, 101);
             int guess = -1;
-            int highGuess = 5;
-            int lowGuess = -5;
+            int closeGuess = 5;
             int guessCounter = 0;
 
             //prompts the user to guess a number
-            Console.WriteLine("I'm thinking of a number between 1 and 100. You have 3 chances to guess it\n");
+            Console.WriteLine("I'm thinking of a number between 0 and 100. You have 3 chances to guess it\n");
+
             //while loop that runs until the guess value is equal to the correct number
             while (guess != rndValue)
             {
                 //Takes the user input and stores it in the guess variable
                 guess = Convert.ToInt32(Console.ReadLine());
-
-                //calculates the value of the difference between the correct number and the guess
-                Math.Abs(rndValue - guess);
 
                 //increase value of guessCounter by one
                 guessCounter++;
@@ -47,12 +44,12 @@ namespace HighLowNumberGuessing
                 }
 
                 //if the difference is between the ranges of the specified ranges let the user know
-                if (Math.Abs(rndValue - guess) >= lowGuess && Math.Abs(rndValue - guess) <= highGuess)
+                if (Math.Abs(rndValue - guess) <= closeGuess)
                 {
                     Console.WriteLine("Your guess is within 5 of the value!\n");
                 }
 
-                if (guessCounter == 3)
+                if (guessCounter >= 3)
                 {
                     Console.WriteLine("\nYou guessed incorrectly too many times. You lose!");
                     break;
